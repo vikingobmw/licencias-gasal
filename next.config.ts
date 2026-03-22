f3cd640c5bd3a5659df63054a736f14ced0a4ff0
vikingobmw <vikingobmw@yahoo.es>
2026-03-22 01:37:53 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options hetypescript: { ignoreBuildErrors: true }, eslint: { ignoreDuringBuilds: true } */
 };
 
 export default nextConfig;
